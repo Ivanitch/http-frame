dd1995e530516f1873ed00acda44d25d728c6b11
S.A.Ivanov <32352214+Ivanitch@users.noreply.github.com>
2020-03-17 22:41:29 +0900

    Added template blocks

--- templates/layout/columns.php
+++ templates/layout/columns.php
@@ -3,18 +3,14 @@
  * @var \Framework\Template\PhpRenderer $this
  */
 ?>
-<?php $this->extend = 'layout/default'; ?>
+
+<?php $this->extend('layout/default'); ?>
 
 <div class="row">
     <div class="col-md-9">
         <?= $content ?>
     </div>
     <div class="col-md-3">
-        <div class="panel panel-default">
-            <div class="panel-heading">Cabinet</div>
-            <div class="panel-body">
-                Cabinet navigation
-            </div>
-        </div>
+        <?= $this->renderBlock('sidebar') ?>
     </div>
 </div>
