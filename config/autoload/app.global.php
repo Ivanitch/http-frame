a3c836cf583e7302a81456a7292e9a70762da786
S.A.Ivanov <32352214+Ivanitch@users.noreply.github.com>
2020-03-17 21:41:48 +0900

    Extracted PHP template renderer

--- config/autoload/app.global.php
+++ config/autoload/app.global.php
@@ -5,37 +5,38 @@ use Framework\Http\Application;
 use Framework\Http\Pipeline\MiddlewareResolver;
 use Framework\Http\Router\AuraRouterAdapter;
 use Framework\Http\Router\Router;
+use Framework\Template\PhpRenderer;
 use Framework\Template\TemplateRenderer;
 use Psr\Container\ContainerInterface;
 
 return [
-    'dependencies' => [
-        'abstract_factories' => [
-            Zend\ServiceManager\AbstractFactory\ReflectionBasedAbstractFactory::class,
-        ],
-        'factories' => [
-            Application::class => function (ContainerInterface $container) {
-                return new Application(
-                    $container->get(MiddlewareResolver::class),
-                    $container->get(Router::class),
-                    new Middleware\NotFoundHandler(),
-                    new Zend\Diactoros\Response()
-                );
-            },
-            Router::class => function () {
-                return new AuraRouterAdapter(new Aura\Router\RouterContainer());
-            },
-            MiddlewareResolver::class => function (ContainerInterface $container) {
-                return new MiddlewareResolver($container);
-            },
-            Middleware\ErrorHandlerMiddleware::class => function (ContainerInterface $container) {
-                return new Middleware\ErrorHandlerMiddleware($container->get('config')['debug']);
-            },
-            TemplateRenderer::class => function () {
-                return new TemplateRenderer('templates');
-            },
-        ],
+  'dependencies' => [
+    'abstract_factories' => [
+      Zend\ServiceManager\AbstractFactory\ReflectionBasedAbstractFactory::class,
     ],
+    'factories' => [
+      Application::class => function (ContainerInterface $container) {
+          return new Application(
+            $container->get(MiddlewareResolver::class),
+            $container->get(Router::class),
+            new Middleware\NotFoundHandler(),
+            new Zend\Diactoros\Response()
+          );
+      },
+      Router::class => function () {
+          return new AuraRouterAdapter(new Aura\Router\RouterContainer());
+      },
+      MiddlewareResolver::class => function (ContainerInterface $container) {
+          return new MiddlewareResolver($container);
+      },
+      Middleware\ErrorHandlerMiddleware::class => function (ContainerInterface $container) {
+          return new Middleware\ErrorHandlerMiddleware($container->get('config')['debug']);
+      },
+      TemplateRenderer::class => function () {
+          return new PhpRenderer('templates');
+      },
+    ],
+  ],
 
-    'debug' => false,
+  'debug' => false,
 ];
--- src/Framework/Template/TemplateRenderer.php
+++ src/Framework/Template/TemplateRenderer.php
@@ -2,24 +2,7 @@
 
 namespace Framework\Template;
 
-class TemplateRenderer
+interface TemplateRenderer
 {
-    private $path;
-
-    public function __construct($path)
-    {
-        if (!is_dir($path))
-            throw new \RuntimeException('Path not found.');
-        $this->path = $path;
-    }
-
-    public function render($view, array $params = []): string
-    {
-        $templateFile = $this->path . '/' . $view . '.php';
-
-        ob_start();
-        extract($params, EXTR_OVERWRITE);
-        require $templateFile;
-        return ob_get_clean();
-    }
+    public function render($name, array $params = []): string;
 }
