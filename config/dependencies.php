71c2b8343caf6eacca28aeeb24e161118f72e335
Ivanitch <amurweb@gmail.com>
2020-03-17 12:58:13 +0900

    Added zend autowiring

--- config/dependencies.php
+++ config/dependencies.php
@@ -1,53 +1,38 @@
 <?php
 
-use App\Http\Action;
 use App\Http\Middleware;
 use Framework\Http\Application;
-use Framework\Http\Middleware\DispatchMiddleware;
-use Framework\Http\Middleware\RouteMiddleware;
 use Framework\Http\Pipeline\MiddlewareResolver;
 use Framework\Http\Router\AuraRouterAdapter;
 use Framework\Http\Router\Router;
 use Psr\Container\ContainerInterface;
 
 return [
-  'dependencies' => [
-    'invokables' => [
-      Middleware\CredentialsMiddleware::class,
-      Middleware\ProfilerMiddleware::class,
-      Action\HelloAction::class,
-      Action\AboutAction::class,
-      Action\CabinetAction::class,
-      Action\Blog\IndexAction::class,
-      Action\Blog\ShowAction::class,
+    'dependencies' => [
+        'abstract_factories' => [
+            Zend\ServiceManager\AbstractFactory\ReflectionBasedAbstractFactory::class,
+        ],
+        'factories' => [
+            Application::class => function (ContainerInterface $container) {
+                return new Application(
+                    $container->get(MiddlewareResolver::class),
+                    $container->get(Framework\Http\Router\Router::class),
+                    new Middleware\NotFoundHandler(),
+                    new Zend\Diactoros\Response()
+                );
+            },
+            Router::class => function () {
+                return new AuraRouterAdapter(new Aura\Router\RouterContainer());
+            },
+            MiddlewareResolver::class => function (ContainerInterface $container) {
+                return new MiddlewareResolver($container);
+            },
+            Middleware\BasicAuthMiddleware::class => function (ContainerInterface $container) {
+                return new Middleware\BasicAuthMiddleware($container->get('config')['users']);
+            },
+            Middleware\ErrorHandlerMiddleware::class => function (ContainerInterface $container) {
+                return new Middleware\ErrorHandlerMiddleware($container->get('config')['debug']);
+            },
+        ],
     ],
-    'factories' => [
-      Application::class => function (ContainerInterface $container) {
-          return new Application(
-            $container->get(MiddlewareResolver::class),
-            $container->get(Framework\Http\Router\Router::class),
-            new Middleware\NotFoundHandler(),
-            new Zend\Diactoros\Response()
-          );
-      },
-      Router::class => function () {
-          return new AuraRouterAdapter(new Aura\Router\RouterContainer());
-      },
-      MiddlewareResolver::class => function (ContainerInterface $container) {
-          return new MiddlewareResolver($container);
-      },
-      Middleware\BasicAuthMiddleware::class => function (ContainerInterface $container) {
-          return new Middleware\BasicAuthMiddleware($container->get('config')['users']);
-      },
-      Middleware\ErrorHandlerMiddleware::class => function (ContainerInterface $container) {
-          return new Middleware\ErrorHandlerMiddleware($container->get('config')['debug']);
-      },
-      DispatchMiddleware::class => function (ContainerInterface $container) {
-          return new DispatchMiddleware($container->get(MiddlewareResolver::class));
-      },
-      RouteMiddleware::class => function (ContainerInterface $container) {
-          return new RouteMiddleware($container->get(Router::class));
-      },
-    ],
-  ],
 ];
