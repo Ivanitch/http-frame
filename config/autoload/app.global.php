dadd0c52713e9fad1f17039eea9a492028d3b5c7
Ivanitch <amurweb@gmail.com>
2020-03-18 15:38:24 +0900

    Added error pages

--- config/autoload/app.global.php
+++ config/autoload/app.global.php
@@ -10,33 +10,36 @@ use Framework\Template\TemplateRenderer;
 use Psr\Container\ContainerInterface;
 
 return [
-  'dependencies' => [
-    'abstract_factories' => [
-      Zend\ServiceManager\AbstractFactory\ReflectionBasedAbstractFactory::class,
+    'dependencies' => [
+        'abstract_factories' => [
+            Zend\ServiceManager\AbstractFactory\ReflectionBasedAbstractFactory::class,
+        ],
+        'factories' => [
+            Application::class => function (ContainerInterface $container) {
+                return new Application(
+                    $container->get(MiddlewareResolver::class),
+                    $container->get(Router::class),
+                    $container->get(Middleware\NotFoundHandler::class),
+                    new Zend\Diactoros\Response()
+                );
+            },
+            Router::class => function () {
+                return new AuraRouterAdapter(new Aura\Router\RouterContainer());
+            },
+            MiddlewareResolver::class => function (ContainerInterface $container) {
+                return new MiddlewareResolver($container);
+            },
+            Middleware\ErrorHandlerMiddleware::class => function (ContainerInterface $container) {
+                return new Middleware\ErrorHandlerMiddleware(
+                    $container->get('config')['debug'],
+                    $container->get(TemplateRenderer::class)
+                );
+            },
+            TemplateRenderer::class => function (ContainerInterface $container) {
+                return new PhpRenderer('templates', $container->get(Router::class));
+            },
+        ],
     ],
-    'factories' => [
-      Application::class => function (ContainerInterface $container) {
-          return new Application(
-            $container->get(MiddlewareResolver::class),
-            $container->get(Router::class),
-            new Middleware\NotFoundHandler(),
-            new Zend\Diactoros\Response()
-          );
-      },
-      Router::class => function () {
-          return new AuraRouterAdapter(new Aura\Router\RouterContainer());
-      },
-      MiddlewareResolver::class => function (ContainerInterface $container) {
-          return new MiddlewareResolver($container);
-      },
-      Middleware\ErrorHandlerMiddleware::class => function (ContainerInterface $container) {
-          return new Middleware\ErrorHandlerMiddleware($container->get('config')['debug']);
-      },
-      TemplateRenderer::class => function (ContainerInterface $container) {
-          return new PhpRenderer('templates', $container->get(Router::class));
-      },
-    ],
-  ],
 
-  'debug' => false,
+    'debug' => true,
 ];
