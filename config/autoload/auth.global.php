f18462410952b272b2077a3182e682c8e528dc16
Ivanitch <amurweb@gmail.com>
2020-03-20 14:58:32 +0900

    Migrated application to PSR middleware

--- config/autoload/auth.global.php
+++ config/autoload/auth.global.php
@@ -2,17 +2,20 @@
 
 use App\Http\Middleware\BasicAuthMiddleware;
 use Psr\Container\ContainerInterface;
+use Zend\Diactoros\Response;
 
 return [
     'dependencies' => [
         'factories' => [
             BasicAuthMiddleware::class => function (ContainerInterface $container) {
-                return new BasicAuthMiddleware($container->get('config')['auth']['users']);
+                return new BasicAuthMiddleware($container->get('config')['auth']['users'], new Response());
             },
         ],
     ],
 
     'auth' => [
-        'users' => ['admin' => 'admin'],
+        'users' => [
+            'admin' => 'admin'
+        ],
     ],
 ];
