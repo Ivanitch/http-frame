c4624f9d1489f07b91e5f9f8c5b511302566da31
S.A.Ivanov <32352214+Ivanitch@users.noreply.github.com>
2020-04-01 16:30:12 +0900

    Performed read model

--- config/autoload/app.global.php
+++ config/autoload/app.global.php
@@ -7,21 +7,19 @@ use Framework\Http\Pipeline\MiddlewareResolver;
 use Framework\Http\Router\Router;
 
 return [
-    'dependencies' => [
-        'abstract_factories' => [
-            Zend\ServiceManager\AbstractFactory\ReflectionBasedAbstractFactory::class,
-        ],
-        'factories' => [
-            Application::class => Infrastructure\Framework\Http\ApplicationFactory::class,
-            Router::class => Infrastructure\Framework\Http\Router\AuraRouterFactory::class,
-            MiddlewareResolver::class => Infrastructure\Framework\Http\Pipeline\MiddlewareResolverFactory::class,
-            ErrorHandlerMiddleware::class => Infrastructure\Framework\Http\Middleware\ErrorHandler\ErrorHandlerMiddlewareFactory::class,
-            ErrorResponseGenerator::class => Infrastructure\Framework\Http\Middleware\ErrorHandler\PrettyErrorResponseGeneratorFactory::class,
-            Psr\Log\LoggerInterface::class => Infrastructure\App\Logger\LoggerFactory::class,
-
-            App\ReadModel\PostReadRepository::class => Infrastructure\App\ReadModel\PostReadRepositoryFactory::class
-        ],
+  'dependencies' => [
+    'abstract_factories' => [
+      Zend\ServiceManager\AbstractFactory\ReflectionBasedAbstractFactory::class,
     ],
+    'factories' => [
+      Application::class => Infrastructure\Framework\Http\ApplicationFactory::class,
+      Router::class => Infrastructure\Framework\Http\Router\AuraRouterFactory::class,
+      MiddlewareResolver::class => Infrastructure\Framework\Http\Pipeline\MiddlewareResolverFactory::class,
+      ErrorHandlerMiddleware::class => Infrastructure\Framework\Http\Middleware\ErrorHandler\ErrorHandlerMiddlewareFactory::class,
+      ErrorResponseGenerator::class => Infrastructure\Framework\Http\Middleware\ErrorHandler\PrettyErrorResponseGeneratorFactory::class,
+      Psr\Log\LoggerInterface::class => Infrastructure\App\Logger\LoggerFactory::class,
+    ],
+  ],
 
-    'debug' => false,
+  'debug' => false,
 ];
