f85872766555fee31335a52aabc0c0739a9ac3b8
S.A.Ivanov <32352214+Ivanitch@users.noreply.github.com>
2020-04-01 15:40:45 +0900

    Changed posts display

--- config/autoload/app.global.php
+++ config/autoload/app.global.php
@@ -7,19 +7,21 @@ use Framework\Http\Pipeline\MiddlewareResolver;
 use Framework\Http\Router\Router;
 
 return [
-    'dependencies' => [
-        'abstract_factories' => [
-            Zend\ServiceManager\AbstractFactory\ReflectionBasedAbstractFactory::class,
-        ],
-        'factories' => [
-            Application::class => Infrastructure\Framework\Http\ApplicationFactory::class,
-            Router::class => Infrastructure\Framework\Http\Router\AuraRouterFactory::class,
-            MiddlewareResolver::class => Infrastructure\Framework\Http\Pipeline\MiddlewareResolverFactory::class,
-            ErrorHandlerMiddleware::class => Infrastructure\Framework\Http\Middleware\ErrorHandler\ErrorHandlerMiddlewareFactory::class,
-            ErrorResponseGenerator::class => Infrastructure\Framework\Http\Middleware\ErrorHandler\PrettyErrorResponseGeneratorFactory::class,
-            Psr\Log\LoggerInterface::class => Infrastructure\App\Logger\LoggerFactory::class,
-        ],
+  'dependencies' => [
+    'abstract_factories' => [
+      Zend\ServiceManager\AbstractFactory\ReflectionBasedAbstractFactory::class,
     ],
+    'factories' => [
+      Application::class => Infrastructure\Framework\Http\ApplicationFactory::class,
+      Router::class => Infrastructure\Framework\Http\Router\AuraRouterFactory::class,
+      MiddlewareResolver::class => Infrastructure\Framework\Http\Pipeline\MiddlewareResolverFactory::class,
+      ErrorHandlerMiddleware::class => Infrastructure\Framework\Http\Middleware\ErrorHandler\ErrorHandlerMiddlewareFactory::class,
+      ErrorResponseGenerator::class => Infrastructure\Framework\Http\Middleware\ErrorHandler\PrettyErrorResponseGeneratorFactory::class,
+      Psr\Log\LoggerInterface::class => Infrastructure\App\Logger\LoggerFactory::class,
 
-    'debug' => false,
+      App\ReadModel\PostReadRepository::class => Infrastructure\App\ReadModel\PostReadRepositoryFactory::class
+    ],
+  ],
+
+  'debug' => false,
 ];
