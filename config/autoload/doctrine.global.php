a8bd8f392f3c842fbcfeb42a7fabac313312218f
S.A.Ivanov <32352214+Ivanitch@users.noreply.github.com>
2020-04-01 16:10:45 +0900

    Enabled Doctrine cache

--- config/autoload/doctrine.global.php
+++ config/autoload/doctrine.global.php
@@ -1,26 +1,40 @@
 <?php
 
 return [
-    'dependencies' => [
-        'factories'  => [
-            Doctrine\ORM\EntityManagerInterface::class => ContainerInteropDoctrine\EntityManagerFactory::class,
-            PDO::class => Infrastructure\App\PDOFactory::class,
-        ],
+  'dependencies' => [
+    'factories'  => [
+      Doctrine\ORM\EntityManagerInterface::class => ContainerInteropDoctrine\EntityManagerFactory::class,
+      PDO::class => Infrastructure\App\PDOFactory::class,
     ],
+  ],
 
-    'doctrine' => [
-        'driver' => [
-            'orm_default' => [
-                'class' => Doctrine\Common\Persistence\Mapping\Driver\MappingDriverChain::class,
-                'drivers' => [
-                    'App\Entity' => 'entities',
-                ],
-            ],
-            'entities' => [
-                'class' => Doctrine\ORM\Mapping\Driver\AnnotationDriver::class,
-                'cache' => 'array',
-                'paths' => ['src/App/Entity'],
-            ],
-        ],
+  'doctrine' => [
+    'configuration' => [
+      'orm_default' => [
+        'result_cache' => 'filesystem',
+        'metadata_cache' => 'filesystem',
+        'query_cache' => 'filesystem',
+        'hydration_cache' => 'filesystem',
+      ],
     ],
+    'driver' => [
+      'orm_default' => [
+        'class' => Doctrine\Common\Persistence\Mapping\Driver\MappingDriverChain::class,
+        'drivers' => [
+          'App\Entity' => 'entities',
+        ],
+      ],
+      'entities' => [
+        'class' => Doctrine\ORM\Mapping\Driver\AnnotationDriver::class,
+        'cache' => 'filesystem',
+        'paths' => ['src/App/Entity'],
+      ],
+    ],
+    'cache' => [
+      'filesystem' => [
+        'class' => Doctrine\Common\Cache\FilesystemCache::class,
+        'directory' => 'var/cache/doctrine',
+      ],
+    ],
+  ],
 ];
